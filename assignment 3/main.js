bfe652e29a2d1a24ca5e390f85ab5d5e28c3e0d4
SaudAli <saud.al.faisal927@gmail.com>
2024-03-27 03:57:15 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//storing the famous quote and its author
+let quote = 'The best activities for your life are PUMPING and HUMPING';
+let author = "SaudAli";
+//prinitng the message
+console.log(quote + ' - ' + author);
---
+++ assignment 3/main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = 'The best activities for your life are PUMPING and HUMPING';
+
+let author:string = "SaudAli";
+
+//prinitng the message
+
+console.log(quote + ' - ' + author);
